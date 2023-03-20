f5f0231184ff0750f19593377fa491d779449f77
齿轮 <2265501251@qq.com>
2023-03-20 16:10:40 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootpetstore/SpringbootPetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootpetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPetstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPetstoreApplication.class, args);
+    }
+
+}
